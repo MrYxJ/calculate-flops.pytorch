cbc4b44a7312f3ed4603da9732f4aa0a82fabfa4
MrYxJ <529674242@qq.com>
2023-09-05 18:05:50 +0800

    update calculate_flops_hf

--- test_examples/test_llm_huggingface.py
+++ test_examples/test_llm_huggingface.py
@@ -8,25 +8,17 @@
  Mail         : yxj2017@gmail.com
  Github       : https://github.com/MrYxJ
  Date         : 2023-09-03 11:21:30
- LastEditTime : 2023-09-05 17:19:56
+ LastEditTime : 2023-09-03 17:29:01
  Copyright (C) 2023 mryxj. All rights reserved.
 '''
 
 from calflops import calculate_flops_hf
 
-
 batch_size = 1
 max_seq_length = 128
-#model_name = "baichuan-inc/Baichuan-13B-Chat"
-model_name = "hfl/chinese-macbert-large"
-
-from calflops import create_empty_model
-model = create_empty_model(model_name, 
-                           library_name='transformers',
-                           trust_remote_code=True,
-                           access_token='')
-print(model.device)
-
+model_name = "baichuan-inc/Baichuan-13B-Chat"
 flops, macs, params = calculate_flops_hf(model_name=model_name,
                                          input_shape=(batch_size, max_seq_length))
 print("%s FLOPs:%s  MACs:%s  Params:%s \n" %(model_name, flops, macs, params))
+
+
